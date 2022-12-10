ed5cdaa487b9ee987b591994a73048c7d6b78960
Joseph Williams L <richlike.joseph@gmail.com>
2022-12-10 00:41:53 -0800

    Print Numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,12 +5,20 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+Int main(void)
 {
 	int a;
 
 	for (a = 0; a < 10; a++)
-		printf("%d", a);
+	{
+		putchar(a + '0');
+		if (a < 9) 
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
+
