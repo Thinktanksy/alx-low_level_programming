5aee32381588bafbd53cbe3dc108cff7034138ef
Joseph Williams L <richlike.joseph@gmail.com>
2022-12-14 04:21:35 -0800

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - returns the sum of its perimeters
+ * @a: int type number
+ * @b: int type number
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
