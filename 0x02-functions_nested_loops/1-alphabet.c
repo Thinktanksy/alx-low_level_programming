232cf8d35aaf8eb19a9492244b4fe666d07cdada
Joseph Williams L <richlike.joseph@gmail.com>
2022-12-13 21:47:11 -0800

    Voi Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Returns: 0
+ */
+
+void print_alphabet(void)
+
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n');
+}
