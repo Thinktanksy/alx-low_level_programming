050e3e299be0e907a73e79b924bb0d48c7bb7fbb
Joseph Williams L <richlike.joseph@gmail.com>
2022-12-14 00:10:28 -0800

    Time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,11 +25,10 @@ void times_table(void)
 					_putchar(' ');
 					_putchar((c % 10) + '0');
 				}
-
 				if (b < 9)
 				{
-					 _putchar(',');
-					 _putchar(' ');
+					_putchar(',');
+					_putchar(' ');
 				}
 			}
 			else
