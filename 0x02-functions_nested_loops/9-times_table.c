abc26cb25c1b95e5d00ec777a2ea17e7c09931fa
Joseph Williams L <richlike.joseph@gmail.com>
2022-12-14 03:14:06 -0800

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,12 +11,12 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_puthar('0');
+		_putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
-			_PUTCHAR(' ');
+			_putchar(' ');
 
 			prod = num * mult;
 
