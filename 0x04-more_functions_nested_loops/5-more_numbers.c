bd665ce0d98cdb27669f2b33bd8b9e29d99ea62d
Joseph Williams L <richlike.joseph@gmail.com>
2022-12-16 13:31:18 -0800

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,6 +2,8 @@
 
 /**
  * more_numbers - print more numbers
+ * _putchar only 3 times
+ * Return: 0-14 10times
  */
 
 void more_numbers(void);
@@ -12,8 +14,8 @@ void more_numbers(void);
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			if (b >= 10)
-				_putchar('1');
+			if (b > 9)
+				_putchar((b / 10) + '0');
 			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
