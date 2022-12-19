05492596ff5d07d63ac7e49e16ea3d2d8cc3c37b
Joseph Williams L <richlike.joseph@gmail.com>
2022-12-19 07:02:02 -0800

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -38,11 +38,13 @@ int _atoi(char *s)
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
-			i++;
 		}
-		if (f == 0)
-			return (0);
-
-		return (n);
+		i++;
 	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
 
