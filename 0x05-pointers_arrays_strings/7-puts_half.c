318d7f852b034af9bbc7488ad6b27cd03c0abaf1
Joseph Williams L <richlike.joseph@gmail.com>
2022-12-19 05:57:01 -0800

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: input
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*str != '\n')
+	{
+		len++;
+		str++;
+	}
+
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar('\n');
+}
