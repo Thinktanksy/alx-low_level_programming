4697bfdc41a1f7fd06629bb7efd2cb751332afe1
Joseph Williams L <richlike.joseph@gmail.com>
2022-12-19 05:11:16 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev -- prints a string, in reverse, followed by a new line
+ * @s: string to print
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+
+	while (s[i] != '\0')
+		i++;
+
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i});
+
+	_putchar('\n');
+}
