9e9b32c593cf640b89e9acb73275b4859549a919
Joseph Williams L <richlike.joseph@gmail.com>
2023-01-03 03:47:47 -0800

    Task 0

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -11,7 +11,7 @@
 void simple_print_buffer(char *buffer, unsigned int size)
 {
 	unsigned int i;
-	
+
 	i = 0;
 	while (i < size)
 	{
@@ -37,7 +37,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
 int main(void)
 {
 	char buffer[98] = {0x00};
-	
+
 	simple_print_buffer(buffer, 98);
 	_memset(buffer, 0x01, 95);
 	printf("-------------------------------------------------\n");
