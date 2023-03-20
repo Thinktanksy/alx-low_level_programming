8c7e7b3cbed17f54c3f140dee70d83a23247a87a
Joseph Williams L <richlike.joseph@gmail.com>
2023-03-20 02:24:29 -0700

    Dynamic Libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-gcc -wall -wextra -werror -pedantic -c -fPIC *.c
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:SLD_LIBRARY_PATH
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
