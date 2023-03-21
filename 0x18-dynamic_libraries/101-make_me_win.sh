45c44adedcc28238c709a46211ffd3a3d2192edc
Joseph Williams L <richlike.joseph@gmail.com>
2023-03-20 23:26:44 -0700

    Dynamic Libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://https://github.com/Thinktanksy/alx-low_level_programming/tree/master/0x18-dynamic_libraries/putshack.so
-export LD_PRELOAD=/tmp/putshack.so
+wget -P /tmp https://github.com/Thinktanksy/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
