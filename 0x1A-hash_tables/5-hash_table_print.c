99e0db4a1c3962202cce4e3aa1a221bef851f864
Joseph Williams L <richlike.joseph@gmail.com>
2023-04-06 09:42:48 -0700

    Hash Table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,6 +9,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
+	unsigned char comma_flag = 0;
 	hash_node_t *tmp;
 
 	if (ht == NULL)
@@ -17,12 +18,17 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		tmp = ht->array[i];
-		while (tmp != NULL)
+		if (ht->array[i] != NULL)
 		{
-			printf("'%s': '%s'", tmp->key, tmp->value);
-			if (tmp != NULL)
+			if (comma_flag == 1)
 				printf(", ");
+
+			tmp = ht->array[i];
+			while (tmp != NULL)
+			{
+				printf("'%s': '%s'", tmp->key, tmp->value);
+				if (tmp != NULL)
+					printf(", ");
 			}
 			comma_flag = 1;
 		}
